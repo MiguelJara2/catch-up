fc95878312cc3413a0845b81d59bab6d1caf90fe
Miguel Jara <u20251856@upc.edu.pe>
2026-04-24 18:12:48 -0500

    feat(footer):  layout

---
+++ src/shared/presentation/components/layout.vue
@@ -0,0 +1,34 @@
+<script setup>
+import {ref} from 'vue';
+import LanguageSwitcher from "./language-switcher.vue";
+import FooterContent from "./footer-content.vue";
+
+const drawerVisible = ref(false);
+const toggleDrawer = () => {
+  drawerVisible.value = !drawerVisible.value;
+};
+</script>
+
+<template>
+<div>
+  <div>
+    <pv-menubar>
+      <template #start>
+        <pv-button icon="pi pi-bars" @click="toggleDrawer" label="CatchUp" text/>
+
+      </template>
+      <template #end>
+        <language-switcher/>
+      </template>
+    </pv-menubar>
+  </div>
+</div>
+  <div>
+
+  </div>
+  <footer-content/>
+</template>
+
+<style scoped>
+
+</style>
