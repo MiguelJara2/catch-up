bd41923e872dd7797693cef96ce04e36381b3a5b
Miguel Jara <u20251856@upc.edu.pe>
2026-04-24 17:27:10 -0500

    feat(material-ui): add primeview ui cmmands component iu

--- src/main.js
+++ src/main.js
@@ -1,5 +1,24 @@
 import { createApp } from 'vue'
 import './style.css'
-import App from './app.vue'
+import App from './app.vue';
+import PrimeVue from 'primevue/config';
+import 'primeflex/primeflex.css';
+import 'primeicons/primeicons.css';
+import Material from '@primeuix/themes/material'
+import {Avatar, Button, Card,Image ,Drawer, Menu, Menubar, SelectButton, Toolbar, Tooltip} from "primevue";
 
-createApp(App).mount('#app')
+
+createApp(App)
+    .use(PrimeVue, {ripple:true,theme:{preset:Material}})
+    .component('pv-button',Button)
+    .component('pv-select-button',SelectButton)
+    .component('pv-avatar',Avatar)
+    .component('pv-card',Card)
+    .component('pv-image',Image)
+    .component('pv-toolbar',Toolbar)
+    .component('pv-menu',Menu)
+    .component('pv-drawer',Drawer)
+    .component('pv-menubar',Menubar)
+    .directive('tooltip',Tooltip)
+    .mount('#app')
+
